c075a2729e96b6052a4296a70639ca949950508f
essasabbagh <essasabbagh@gmail.com>
2021-07-05 07:56:29 +0300

    addnew code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 import 'package:real_shop/Providers/auth.dart';
 import 'package:real_shop/Providers/cart.dart';
 import 'package:real_shop/Providers/orders.dart';
@@ -20,22 +21,29 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
     return MultiProvider(
         providers: [
           ChangeNotifierProvider.value(value: Auth()),
           ChangeNotifierProxyProvider<Auth, Products>(
-            create: (_) => Products(),
-            update: (ctx, authValue, previousProducts) => previousProducts
-              ..gtData(authValue.token, authValue.userId,
-                  previousProducts == null ? null : previousProducts.items),
-          ),
+              create: (_) => Products(),
+              update: (ctx, authValue, previousProducts) => previousProducts
+                ..gtData(authValue.token, authValue.userId,
+                    previousProducts == null ? null : previousProducts.items)),
           ChangeNotifierProvider.value(value: Cart()),
           ChangeNotifierProxyProvider<Auth, Orders>(
-            create: (_) => Orders(),
-            update: (ctx, authValue, previousOrders) => previousOrders
-              ..gtData(authValue.token, authValue.userId,
-                  previousOrders == null ? null : previousOrders.orders),
-          ),
+              create: (_) => Orders(),
+              update: (ctx, authValue, previousOrders) => previousOrders
+                ..gtData(authValue.token, authValue.userId,
+                    previousOrders == null ? null : previousOrders.orders)),
         ],
         child: Consumer<Auth>(
           builder: (ctx, auth, _) => MaterialApp(
@@ -45,7 +53,10 @@ class MyApp extends StatelessWidget {
                 primarySwatch: Colors.blue,
                 accentColor: Colors.deepOrange,
                 fontFamily: 'Lato'),
-            home: !auth.isAuth
+            home:
+            //  MyHomePage(),
+
+             auth.isAuth
                 ? ProductOverviewScreen()
                 : FutureBuilder(
                     future: auth.tryAutoLogin(),
@@ -70,32 +81,27 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController myAnimation;
+  @override
+  void initState() {
+    super.initState();
+    myAnimation =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Real Shop'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+        body: Center(
+            child: InkWell(
+                onTap: () => myAnimation.isCompleted
+                    ? myAnimation.reverse()
+                    : myAnimation.forward(),
+                child: AnimatedIcon(
+                    icon: AnimatedIcons.ellipsis_search,
+                    size: 100,
+                    progress: myAnimation))));
   }
 }
