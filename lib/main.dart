4daf84c421982682ba460a49dfe2b109bda16bcb
isa-sabbagh-2P <i.sabbgah@tawasulforum.org>
2021-07-05 17:59:39 +0300

    add some animation packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:like_button/like_button.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
 import 'package:real_shop/Providers/auth.dart';
@@ -7,6 +9,7 @@ import 'package:real_shop/Providers/orders.dart';
 import 'package:real_shop/Providers/product.dart';
 import 'package:real_shop/Screens/product_detail_screen.dart';
 import 'package:real_shop/Screens/splash_screen.dart';
+import 'package:slide_popup_dialog/slide_popup_dialog.dart' as slideDialog;
 
 import 'Providers/products.dart';
 import 'Screens/auth_screen.dart';
@@ -53,14 +56,16 @@ class MyApp extends StatelessWidget {
                 primarySwatch: Colors.blue,
                 accentColor: Colors.deepOrange,
                 fontFamily: 'Lato'),
-            home: auth.isAuth
-                ? ProductOverviewScreen()
-                : FutureBuilder(
-                    future: auth.tryAutoLogin(),
-                    builder: (ctx, authSnapshot) =>
-                        authSnapshot.connectionState == ConnectionState.waiting
-                            ? SplashScreen()
-                            : AuthScreen()),
+            home: MyHomePage(),
+
+            // auth.isAuth
+            //     ? ProductOverviewScreen()
+            //     : FutureBuilder(
+            //         future: auth.tryAutoLogin(),
+            //         builder: (ctx, authSnapshot) =>
+            //             authSnapshot.connectionState == ConnectionState.waiting
+            //                 ? SplashScreen()
+            //                 : AuthScreen()),
             routes: {
               ProductDetailScreen.routeName: (_) => ProductDetailScreen(),
               CartScreen.routeName: (_) => CartScreen(),
@@ -73,34 +78,89 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-// class _MyHomePageState extends State<MyHomePage>
-//     with SingleTickerProviderStateMixin {
-//   AnimationController myAnimation;
-//   @override
-//   void initState() {
-//     super.initState();
-//     myAnimation =
-//         AnimationController(vsync: this, duration: Duration(milliseconds: 500));
-//     Provider.of<Auth>(context).isAuth;
-//     print('main->isAuth :${Provider.of<Auth>(context).isAuth}');
-//   }
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController myAnimation;
+  @override
+  void initState() {
+    super.initState();
+    myAnimation =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    // Provider.of<Auth>(context).isAuth;
+    // print('main->isAuth :${Provider.of<Auth>(context).isAuth}');
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         body: Center(
-//             child: InkWell(
-//                 onTap: () => myAnimation.isCompleted
-//                     ? myAnimation.reverse()
-//                     : myAnimation.forward(),
-//                 child: AnimatedIcon(
-//                     icon: AnimatedIcons.ellipsis_search,
-//                     size: 100,
-//                     progress: myAnimation))));
-//   }
-// }
+  void _showDialog() {
+    slideDialog.showSlideDialog(
+      context: context,
+      child: Text("Hello World"),
+      barrierColor: Colors.white.withOpacity(0.7),
+      pillColor: Colors.blueGrey,
+      backgroundColor: Colors.amber[50],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: ElevatedButton(
+                child: Text("Press to open dialog"), onPressed: _showDialog)
+
+            // child: SpinKitChasingDots(
+            //   color: Colors.blue,
+            //   size: 50.0,
+            //   duration: Duration(seconds: 2),
+            //   // controller: AnimationController(
+            //   //   vsync: this,
+            //   //   duration: const Duration(milliseconds: 1200),
+            //   // ),
+            // ),
+//
+            // child: LikeButton(
+            //   size: 40,
+            //   circleColor: CircleColor(start: Colors.amber, end: Colors.orangeAccent),
+            //   bubblesColor: BubblesColor(
+            //     dotPrimaryColor: Colors.red,
+            //     dotSecondaryColor: Colors.deepOrange,
+            //   ),
+            //   likeBuilder: (bool isLiked) {
+            //     return Icon(
+            //       Icons.favorite,
+            //       color: isLiked ? Colors.redAccent : Colors.grey,
+            //       size: 40,
+            //     );
+            //   },
+            //   likeCount: 665,
+            //   onTap: _onLikeTapped,
+            //   countBuilder: (int count, bool isLiked, String text) {
+            //     var color = isLiked ? Colors.redAccent : Colors.grey;
+            //     Widget result;
+            //     if (count == 0) {
+            //       result = Text("love", style: TextStyle(color: color));
+            //     } else
+            //       result = Text(text, style: TextStyle(color: color));
+            //     return result;
+            //   },
+            // ),
+
+//
+
+            // child: InkWell(
+            //     onTap: () => myAnimation.isCompleted
+            //         ? myAnimation.reverse()
+            //         : myAnimation.forward(),
+            //     child: AnimatedIcon(
+            //         icon: AnimatedIcons.ellipsis_search,
+            //         size: 100,
+            //         progress: myAnimation))
+            ));
+  }
+
+  Future<bool> _onLikeTapped(bool isLiked) async => !isLiked;
+}
